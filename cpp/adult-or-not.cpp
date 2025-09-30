e639d930a52310c6d50d0396c9169d992f19cc05
SH20RAJ <shaswatraj3@gmail.com>
2025-09-30 23:12:10 +0530

    Add adult-or-not program and twoSum implementation with tests

---
+++ cpp/adult-or-not.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    cin >> age;
+    if(age>18){
+        cout << "Adult";
+    } else {
+        cout << "Kid";
+    }
+    
+}
