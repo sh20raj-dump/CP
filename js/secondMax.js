d56b849c79a303b00abee0879f8516c9bef178ea
SH20RAJ <shaswatraj3@gmail.com>
2025-12-28 16:38:39 +0530

    feat: Add C++ stack implementation, JS second max function, and C++ linked list boilerplate.

---
+++ js/secondMax.js
@@ -0,0 +1,7 @@
+let arr = [3, 5, 7, 2, 8, 6, 8, 5];
+
+function findSecondMax(array) {
+    
+}
+
+console.log(findSecondMax(arr)); // Output: 7
---
+++ stack/stack.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack {
+  vector<int> v;
+
+public:
+  void push(int val) { return v.push_back(val); };
+
+  int pop() {
+    v.pop_back();
+    return this->top();
+  };
+
+  int top() { return v.at(v.size() - 1); };
+
+  bool isEmpty() { return v.size() == 0; };
+};
+
+int main() {
+  Stack s;
+
+  s.push(1);
+  s.push(12);
+  s.push(13);
+  
+  while (!s.isEmpty()) {
+    cout << s.top() << " ";
+    s.pop();
+  }
+  cout << endl;
+
+  return 0;
+}
