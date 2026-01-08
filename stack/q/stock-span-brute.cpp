e5d92f916e3f95802ac2eb0ea3978121f639dee2
SH20RAJ <shaswatraj3@gmail.com>
2026-01-08 17:49:46 +0530

    stack

---
+++ stack/q/stock-span-brute.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int arr[] = {100,80,60,70,75,85};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int ss[size];
+
+
+
+
+    int curr=0;
+
+    for(int i = 0; i < size; i++){
+        cout << i;
+    }
+
+
+
+
+
+
+    // result = [1,1,1,2,1,4,6]
+
+    return 0;
+}
